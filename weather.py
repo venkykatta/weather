2a35f25f21567eaecc5bdae90bdd0f5cfe8ef806
Venky <venkateshkatta9705@gmail.com>
2020-08-01 00:27:53 +0530

    changes

--- weather.py
+++ weather.py
@@ -5,19 +5,20 @@ app = Flask(__name__,template_folder='template')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html")    
 
 
 @app.route('/', methods=['POST'])
 def Onsubmit():
     place = request.form['place']
-    place_in_uppercase = place.upper()
+    # place_in_uppercase = place.upper()
     API_KEY = 'e03b0e4011564a1a9e11eb7086d70d1e'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={place}&APPID={API_KEY}&units=metric'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={place }&APPID={API_KEY}&units=metric'
     weather_info = requests.get(url).json()
     temparature = weather_info["main"]["temp"]
     temp_in_string = str(temparature)
-    return f'The current temparature in {place_in_uppercase} is {temp_in_string} \u00b0C'
+    # return f'The current temparature in {place_in_uppercase} is {temp_in_string} \u00b0C'
+    return f'The current temparature in {place} is {temp_in_string} \u00b0C'
     
 if __name__ == "__main__":
     app.run(debug=True)
